4e0e0befe1e51766796c5910bcab1eccec3a442a
l3851 <8315181+l3851@user.noreply.gitee.com>
2020-11-24 09:34:42 +0800

    注释

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>
   <div id="app">
-    <h1>黑马头条</h1>
-    <div>
-    <i class="toutiao toutiao-shipin"></i>
-    </div>
-    <van-button type="default">默认按钮</van-button>
-<van-button type="primary">主要按钮</van-button>
-<van-button type="info">信息按钮</van-button>
-<van-button type="warning">警告按钮</van-button>
-<van-button type="danger">危险按钮</van-button>
-
-<!--单元格-->
-<van-cell-group>
-  <van-cell title="单元格" value="内容" />
-  <van-cell title="单元格" value="内容" label="描述信息" />
-</van-cell-group>
-    <router-view />
+    <!-- 路由的出口 -->
+    <router-view/>
   </div>
 </template>
 
@@ -25,4 +11,4 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style lang="less"></style>
